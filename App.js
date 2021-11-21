0ab17b238b8ad2041dba0f7ce3fd36015752a2f3
Lakshita6 <65706730+Lakshita6@users.noreply.github.com>
2021-11-22 01:37:45 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import DataTable from './Components/DataTable';
+
+function App() {
+  return (
+    <div className="App">
+    <h1>Question one by Lakshita:</h1>
+      <DataTable></DataTable>
+      
+    </div>
+  );
+}
+
+export default App;
